3f816b1cb412f7caef703ae34820f7cd2f79b82f
wenewzhang <wenewboy@gmail.com>
2019-01-30 11:39:59 +0800

    import package

---
+++ config/config.go
@@ -0,0 +1,23 @@
+package config
+const (
+	UserId    = "ed882a39-0b0c-4413-bbd9-221cdeee56bf"
+	PinCode   = "910147"
+	SessionId = "0be3419c-760b-4d45-bac7-6351d74f21f6"
+	PinToken  = "GFYyRh1DeIgj/qo5i4IILAM/n6Qn7Uqw2PU9kpMGyz+diXcpdEdJoiCLTmC+TwGkXbQ+g3f8fKUH9X+KcOr5pHMSgYKs0wCzfmFUHSt9upUqkhYrUpDGdwPsoYttMO0tymCFuP29wAFB5A6PeI9dLob3IC+IfbEugkUBR7t2UL0="
+	//please delele the blank of PrivateKey the before each line
+	PrivateKey = `-----BEGIN RSA PRIVATE KEY-----
+MIICXAIBAAKBgQCjpYA6NMBzda/oTfUZPWCwUMv+orjpczvd6mu6QXS0NGEOJWYd
+5rxPR9uqpeAc8k45LgxpwAHgQw9sBinTyObFZJvLUlGmw3dKZkWum35wJmS9QrdP
+Uqa7UyQUcaI8Iwkgx9rhnBWlAhOrM9rsw7mMjjvtNeUe4lkeSQeTk5fMKQIDAQAB
+AoGASph8lQvK1lbFMs2mdjTmSDjsZExkofXnSkc7iBGiUyq1Oy+NFwBAwprs5SLr
+wAYr5iC5g9MEh+fJteIxWtrvZjzKVWdKES4t2iRF5Bhh7QjkbUud9dzZ35Sd2hzp
+u0/UC4j/dEFmU9Z9IkjgUPSVBYmJ/DQ5Kf00I20NdMz9nwECQQDRSCgYMG/s+CjI
+XF0xDBuuHR+r/OQibomYXj8UDflKTWwBRhYLbgMKKDfZe+JWB5Dpr3dvpjt3rEyr
+so50BThZAkEAyC1qsJ0fY91OMYJYiwtMIimjcyedDgwcJdfBFX7NNfBPZEP1ZKeo
+B01J3DnD/4LVYm6OKTFHR8v9FcrH6tm4UQJABsHRqZZGxtvsVSvGRoMhkamXEDKG
+GyZ+gtDoMHmTBvcg+ZmNtKjEqFtMElWRpT1xTFpB/PTdDqwdfZ2yQ8S3YQJAK6m0
+Zteu3CKOhsAOvi11qtH4RAr09/ZuQVbhV+Xzrvs+5fjo0s67GPSj0rSK6vXHw5On
+0JZeHOwCxTkZCEH7MQJBAJHiO4ozWGejeXPJFG/KQfnMtkfR4sQQYO0si3FQN6Es
+B+0+m2lVMLF/zat12R2WYgFBFkFEcpphvR7Dg6V1cfw=
+-----END RSA PRIVATE KEY-----`
+)
