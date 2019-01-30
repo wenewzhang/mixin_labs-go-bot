951d8a8391c14f02184c6756f513a4d71b90619e
wenewzhang <wenewboy@gmail.com>
2019-01-30 12:28:01 +0800

    base64,set gopath

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # mixin_labs-go-bot
 go bot for Mixin Network
+Download go from [Go](https://golang.org/dl/)
+```bash
+echo `export GOPATH=$HOME/go` >> ~/.bash_profile
+
+```
---
+++ pintoken/pintoken.go
@@ -0,0 +1,9 @@
+package main
+import (
+  // "encoding/base64"
+  "fmt"
+  config "github.com/wenewzhang/mixin_labs-go-bot/config"
+)
+func main() {
+  fmt.Println(config.PinToken);
+}
