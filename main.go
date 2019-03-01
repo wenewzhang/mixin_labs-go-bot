9899357274c6e3057344612f7fd3f0c14cac2d7f
wenewzhang <wenewboy@gmail.com>
2019-03-01 10:22:38 +0800

    app card & app button group

--- main.go
+++ main.go
@@ -31,7 +31,38 @@ func (l *Listener) OnMessage(ctx context.Context, msg messenger.MessageView, use
 		// return l.SendPlainText(ctx, msg.ConversationId, msg.UserId, string(data))
 	} else if msg.Category == "PLAIN_TEXT" {
 		log.Printf("I got a message, it said: %s", string(data))
-		return l.SendPlainText(ctx, msg.ConversationId, msg.UserId, string(data))
+		if string(data) == "g" {
+			payLinkEOS := "https://mixin.one/pay?recipient=" +
+							 msg.UserId  + "&asset=" +
+							 "6cfe566e-4aad-470b-8c9a-2fd35b49c68d"   +
+							 "&amount=" + "0.1" +
+							 "&trace="  + messenger.UuidNewV4().String() +
+							 "&memo=";
+		  payLinkBTC := "https://mixin.one/pay?recipient=" +
+							 msg.UserId  + "&asset=" +
+							 "c6d0c728-2624-429b-8e0d-d9d19b6592fa"   +
+							 "&amount=" + "0.001" +
+							 "&trace="  + messenger.UuidNewV4().String() +
+							 "&memo=";
+		  log.Println(payLinkBTC)
+			google := messenger.Button{Label: "Pay EOS 0.1", Color: "#0080FF", Action: payLinkEOS}
+			baidu  := messenger.Button{Label: "Pay BTC 0.001", Color: "#00FF80", Action: payLinkBTC}
+			if err := l.SendAppButtons(ctx, msg.ConversationId, msg.UserId, google, baidu); err != nil {
+				return err
+			}
+			return nil
+		} else if string(data) == "a"  {
+			card := messenger.AppCard{Title: "CNB", Description: "Chui Niu Bi", Action: "http://www.google.cn",
+				IconUrl: "https://images.mixin.one/0sQY63dDMkWTURkJVjowWY6Le4ICjAFuu3ANVyZA4uI3UdkbuOT5fjJUT82ArNYmZvVcxDXyNjxoOv0TAYbQTNKS=s128"}
+			if err := l.SendAppCard(ctx, msg.ConversationId, msg.UserId, card); err != nil {
+				return err
+			}
+			return nil
+		} else if string(data) == "r" {
+			mixin.Transfer(msg.UserId,"0.0001","c6d0c728-2624-429b-8e0d-d9d19b6592fa","",messenger.UuidNewV4().String(),
+										PinCode,PinToken,UserId,SessionId,PrivateKey)
+			return nil
+		} else { return l.SendPlainText(ctx, msg.ConversationId, msg.UserId, string(data)) }
 	} else {
 		log.Println("Unknown message!", msg.Category)
 		return err
